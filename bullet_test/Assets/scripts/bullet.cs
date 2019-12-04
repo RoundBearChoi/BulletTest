ff7f76f51562989d834b632739f399e1433f61ea
RoundBearChoi <roundbeargames@gmail.com>
2019-12-04 21:37:07 +0900

    choi

---
+++ bullet_test/Assets/scripts/bullet.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+    public GameObject ParticlePrefab;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Explode();
+    }
+
+    void Explode()
+    {
+        Instantiate(ParticlePrefab, this.transform.position, Quaternion.identity);
+        Destroy(this.gameObject);
+    }
+
+    IEnumerator Start()
+    {
+        yield return new WaitForSeconds(3f);
+        Destroy(this.gameObject);
+    }
+}
---
+++ bullet_test/Assets/scripts/gun.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gun : MonoBehaviour
+{
+    public GameObject BulletPrefab;
+    public float Force;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Fire();
+        }
+    }
+
+    void Fire()
+    {
+        GameObject obj = Instantiate(BulletPrefab, this.transform.position, Quaternion.identity);
+        obj.GetComponent<Rigidbody>().AddForce(new Vector3(Force, 0f, 0f));
+    }
+}
